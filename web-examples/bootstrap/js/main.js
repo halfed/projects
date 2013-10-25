ac637638ce3f10d840e949b0e2d2c6a48ab930fd
halfed <1half.ed@gmail.com>
2013-10-24 19:57:54 -0600

    my first commit

---
+++ web-examples/bootstrap/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	$('#slides').slidesjs({
+    	width: 731,
+        height: 440,
+        play: {
+          active: false,
+          auto: true,
+          interval: 6000,
+          swap: false,
+		  effect: "fade"
+        }
+	});
+});
